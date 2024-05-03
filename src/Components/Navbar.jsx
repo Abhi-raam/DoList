883fe1cc97efd79c0b39f933cf32ad7c02bde6bb
Abhiram Suresh <abhiramunni246@gmail.com>
2024-05-04 01:13:49 +0530

    nav btn function changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+    const [navbtn, setNavBtn] = useState(false)
+    const toggleNavBtn = () => {
+        setNavBtn(!navbtn)
+    }
     return (
         <div className='shadow-md'>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    <div className="">
+                        <div className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" onClick={toggleNavBtn} className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         {/* mobile view */}
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to='/'>Home</Link></li>
-                            <li>
-                                <a href="">Projects</a>
-                                <ul className="p-2">
-                                    <li><Link to='/projects'>All Projects</Link></li>
-                                    <li><Link to='/add-projects'>Add Projects</Link></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
-                        </ul>
+                        {navbtn ?
+                        <div className='relative'>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 absolute  ">
+                                <li><Link to='/'>Home</Link></li>
+                                <li>
+                                    <a href="">Projects</a>
+                                    <ul className="p-2">
+                                        <li><Link to='/projects'>All Projects</Link></li>
+                                        <li><Link to='/add-projects'>Add Projects</Link></li>
+                                    </ul>
+                                </li>
+                                <li><a>Item 3</a></li>
+                            </ul>
+                        </div>
+                            : null}
                     </div>
                     <a className="btn btn-ghost text-xl text-[#7365b7]">DoList</a>
                 </div>

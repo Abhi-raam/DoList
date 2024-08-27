6fc3115b14f9714d1101e2246c98434aea785041
Abhiram Suresh <abhiramunni246@gmail.com>
2024-08-27 10:36:29 +0530

    footer position changed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className='fixed left-0 bottom-0 w-full'>
+        <div className=''>
             <footer className="footer footer-center font-medium bg-[#e5dbfe] text-base-content p-4">
                 <aside>
                     <p >Copyright © 2024 - All right reserved</p>
